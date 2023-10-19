d9187689b8abd288a8189da7916f2c2f072abf86
DanielKirilov <dancata08@gmail.com>
2023-10-19 21:20:44 +0300

    Added express, handlebars and added folders

---
+++ JS-Back-End/examPrep/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const handlebars = require("express-handlebars")
+const path = require("path");
+
+const routes = require("./routes")
+
+const app = express();
+
+app.engine("hbs", handlebars.engine({
+    extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", "src/views")
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.urlencoded({extended: false}));
+app.use(routes)
+
+app.listen(5000, console.log("Server is listening on port 5000"))
