abb51062c8a050fc7dcabf520ce832c4de33cf1f
DanielKirilov <dancata08@gmail.com>
2023-10-19 23:38:00 +0300

    jwt logic

--- JS-Back-End/examPrep/src/managers/userManager.js
+++ JS-Back-End/examPrep/src/managers/userManager.js
@@ -1,7 +1,21 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-exports.login = (username, password) => {
-  //
+exports.login = async (username, password) => {
+  //find user
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    throw new Error("Invalid user or password!");
+  }
+  //check pass
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    throw new Error("Invalid user or password!");
+  }
+
+  
 };
 
 exports.register = async (userData) => {
