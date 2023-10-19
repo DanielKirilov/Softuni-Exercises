141f4386904383fdbb81c323fc8c63f78a0ae32a
DanielKirilov <dancata08@gmail.com>
2023-10-19 22:18:18 +0300

    Fixed login page

--- JS-Back-End/examPrep/src/controllers/userController.js
+++ JS-Back-End/examPrep/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/login", (req, res) => {
-        res.render("/users/login")
-})
+  res.render("users/login");
+});
 
-module.exports = router
+module.exports = router;
