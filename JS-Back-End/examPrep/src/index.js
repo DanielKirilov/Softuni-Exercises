c5eeb33383c883362748aa0ecc10bc81d97e5945
DanielKirilov <dancata08@gmail.com>
2023-10-19 21:37:58 +0300

    Added and connected mongoose

--- JS-Back-End/examPrep/src/index.js
+++ JS-Back-End/examPrep/src/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const handlebars = require("express-handlebars")
 const path = require("path");
+const mongoose = require("mongoose")
 
 const routes = require("./routes")
 
 const app = express();
 
+// TODO: CHANGE DATABASE NAME
+mongoose.connect("mongodb://127.0.0.1:27017/petstagram")
+.then(() => console.log("DB conncted successfully"))
+.catch(err => console.log("DataBase error: ", err))
 app.engine("hbs", handlebars.engine({
     extname: "hbs"
 }));
 app.set("view engine", "hbs");
 app.set("views", "src/views")
+
+
 
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({extended: false}));
