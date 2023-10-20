f22b785d3e2d73714df40e357ec6e83cb4aba061
DanielKirilov <dancata08@gmail.com>
2023-10-20 22:51:26 +0300

    Added photo model and changed port

---
+++ JS-Back-End/examPrep/src/models/Photo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const photoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+//   commentList: {
+//     type: String,
+//     required: true,
+//   },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
