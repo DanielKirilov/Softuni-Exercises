d7873334e000148a9ea80bbbf5dca63c5768db3e
DanielKirilov <dancata08@gmail.com>
2023-10-20 21:09:15 +0300

    Updated error handling

--- JS-Back-End/examPrep/src/controllers/userController.js
+++ JS-Back-End/examPrep/src/controllers/userController.js
@@ -1,36 +1,44 @@
 const router = require("express").Router();
-const userManager = require("../managers/userManager")
+const userManager = require("../managers/userManager");
 const { TOKEN_KEY } = require("../config/config");
+const { getErrorMessage } = require("../utils/errorHelpers");
 
 router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
 router.post("/login", async (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
 
-  const token = await userManager.login(username, password); 
+  try {
+    const token = await userManager.login(username, password);
 
-  res.cookie(TOKEN_KEY, token);
+    res.cookie(TOKEN_KEY, token);
 
-  res.redirect("/")
-})
+    res.redirect("/");
+  } catch (err) {
+    res.render("users/login", {error: getErrorMessage})
+  }
+});
 
 router.get("/register", (req, res) => {
-    res.render("users/register");
-  });
+  res.render("users/register");
+});
 
 router.post("/register", async (req, res) => {
-  const {username, email, password, repeatPassword} = req.body;
+  const { username, email, password, repeatPassword } = req.body;
 
-  await userManager.register({username, email, password, repeatPassword});
-
-  res.redirect("/users/login")
-})
+  try {
+    await userManager.register({ username, email, password, repeatPassword });
+    res.redirect("/users/login");
+  } catch (err) {
+    res.render("users/register", { error: getErrorMessage(err) });
+  }
+});
 
 router.get("/logout", async (req, res) => {
   res.clearCookie("token");
 
-  res.redirect("/")
-})
+  res.redirect("/");
+});
 module.exports = router;
--- JS-Back-End/examPrep/src/middlewares/errorHandlerMiddleware.js
+++ JS-Back-End/examPrep/src/middlewares/errorHandlerMiddleware.js
@@ -1,4 +1,4 @@
-const { getErrorMessage } = require("../utils/errorHeleps");
+const { getErrorMessage } = require("../utils/errorHelpers");
 
 exports.errorHandler = (err, req, res) => {
   res.render("/404", {erorr: getErrorMessage(err)})  
